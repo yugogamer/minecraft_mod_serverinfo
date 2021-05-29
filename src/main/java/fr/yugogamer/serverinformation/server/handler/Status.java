65d002a9662c4667b8ef66cb05534ec9dbd5baea
Yugogamer <yugogamer@gmail.com>
2021-05-29 23:51:07 +0200

    Update Status.java
    add information

--- src/main/java/fr/yugogamer/serverinformation/server/handler/Status.java
+++ src/main/java/fr/yugogamer/serverinformation/server/handler/Status.java
@@ -16,18 +16,20 @@ public class Status extends MinecraftHandler {
     public void handle(HttpExchange httpExchange) throws IOException {
         JsonObject response = new JsonObject();
         response.addProperty("name",this.minecraftServer.getName());
-        response.addProperty("version",this.minecraftServer.getMOTD());
+        response.addProperty("version",this.minecraftServer.getMinecraftVersion());
+        response.addProperty("description",this.minecraftServer.getMOTD());
         response.addProperty("maxplayer",this.minecraftServer.getMaxPlayers());
         response.addProperty("currentplayer", this.minecraftServer.getPlayerList().getCurrentPlayerCount());
         response.addProperty("whitelist", this.minecraftServer.getPlayerList().isWhiteListEnabled());
         response.addProperty("modded",this.minecraftServer.getServerConfiguration().isModded());
         response.addProperty("hardcore", this.minecraftServer.getServerConfiguration().isHardcore());
         response.addProperty("gametype",this.minecraftServer.getServerConfiguration().getGameType().getName());
-        response.addProperty("pvp",this.minecraftServer.isPVPEnabled());
-
-        String sendToClient = response.getAsString();
+        response.addProperty("pvp",this.minecraftServer.isPVPEnabled());    
+        String sendToClient = response.toString();
         httpExchange.sendResponseHeaders(200, sendToClient.length());
         OutputStream os = httpExchange.getResponseBody();
+        os.write(sendToClient.getBytes());
+        os.write(sendToClient.getBytes());
         os.write(sendToClient.getBytes());
         os.close();
     }
