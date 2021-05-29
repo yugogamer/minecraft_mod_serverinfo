acbab74241130b7052d3ac01c5850032dcfec1af
Yugogamer <yugogamer@gmail.com>
2021-05-30 01:06:41 +0200

    Update MinecraftHandler.java
    adding a query to map method

--- src/main/java/fr/yugogamer/serverinformation/server/handler/MinecraftHandler.java
+++ src/main/java/fr/yugogamer/serverinformation/server/handler/MinecraftHandler.java
@@ -5,6 +5,8 @@ import com.sun.net.httpserver.HttpHandler;
 import net.minecraft.server.MinecraftServer;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MinecraftHandler implements HttpHandler {
     protected MinecraftServer minecraftServer;
@@ -16,5 +18,21 @@ public class MinecraftHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
 
+    }
+
+    protected Map<String, String> queryToMap(String query) {
+        if(query == null) {
+            return null;
+        }
+        Map<String, String> result = new HashMap<>();
+        for (String param : query.split("&")) {
+            String[] entry = param.split("=");
+            if (entry.length > 1) {
+                result.put(entry[0], entry[1]);
+            }else{
+                result.put(entry[0], "");
+            }
+        }
+        return result;
     }
 }
