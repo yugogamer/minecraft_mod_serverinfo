c78812081e826421c1648954b5ea704fe402cfed
Yugogamer <yugogamer@gmail.com>
2021-05-30 01:30:17 +0200

    Create SendMessage.java
    adding handler to send message

---
+++ src/main/java/fr/yugogamer/serverinformation/server/handler/SendMessage.java
@@ -0,0 +1,45 @@
+package fr.yugogamer.serverinformation.server.handler;
+
+import com.google.gson.JsonObject;
+import com.sun.net.httpserver.HttpExchange;
+import net.minecraft.entity.player.ServerPlayerEntity;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.text.StringTextComponent;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+public class SendMessage extends MinecraftHandler {
+    public SendMessage(MinecraftServer minecraftServer) {
+        super(minecraftServer);
+    }
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        JsonObject response = new JsonObject();
+        String query = httpExchange.getRequestURI().getQuery();
+        Map<String,String> querys = this.queryToMap(query);
+        if (querys.get("username") != null && querys.get("message") != null)
+        {
+            List<ServerPlayerEntity> litPlayer;
+            litPlayer = this.minecraftServer.func_241755_D_().getPlayers();
+            String message = querys.get("username") + " : " + querys.get("message");
+            for (ServerPlayerEntity playerEntity : litPlayer) {
+                playerEntity.sendMessage(new StringTextComponent(message), UUID.randomUUID());
+            }
+            response.addProperty("status","succes");
+        }else
+        {
+            response.addProperty("status","error");
+            response.addProperty("reason","query missing");
+        }
+        String sendToClient = response.toString();
+        httpExchange.sendResponseHeaders(200, sendToClient.length());
+        OutputStream os = httpExchange.getResponseBody();
+        os.write(sendToClient.getBytes());
+        os.close();
+    }
+}
