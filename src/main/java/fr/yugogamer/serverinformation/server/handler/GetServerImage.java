33099af6b4b0e196237089753a2c5ea36c72303d
Yugogamer <yugogamer@gmail.com>
2021-05-30 19:27:27 +0200

    Create GetServerImage.java
    allow to get image of the server

---
+++ src/main/java/fr/yugogamer/serverinformation/server/handler/GetServerImage.java
@@ -0,0 +1,34 @@
+package fr.yugogamer.serverinformation.server.handler;
+
+import com.google.gson.JsonObject;
+import com.sun.net.httpserver.HttpExchange;
+import fr.yugogamer.serverinformation.config.Config;
+import net.minecraft.server.MinecraftServer;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
+import java.awt.image.WritableRaster;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+
+public class GetServerImage  extends MinecraftHandler {
+    public GetServerImage(MinecraftServer minecraftServer) {
+        super(minecraftServer);
+    }
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+
+        File file = this.minecraftServer.getWorldIconFile();
+
+        httpExchange.sendResponseHeaders(200, file.length());
+        httpExchange.getResponseHeaders().add("Content-Type", "image/png");
+
+        OutputStream outputStream=httpExchange.getResponseBody();
+        Files.copy(file.toPath(), outputStream);
+        outputStream.close();
+    }
+}
