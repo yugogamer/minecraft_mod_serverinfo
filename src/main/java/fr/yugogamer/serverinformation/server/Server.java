7c1f62ad888515525839f7906201fccd20bbc71c
Yugogamer <yugogamer@gmail.com>
2021-05-30 03:12:46 +0200

    Update Server.java
    change port for a new, using the Config systeme

--- src/main/java/fr/yugogamer/serverinformation/server/Server.java
+++ src/main/java/fr/yugogamer/serverinformation/server/Server.java
@@ -3,6 +3,7 @@ package fr.yugogamer.serverinformation.server;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import fr.yugogamer.serverinformation.config.Config;
 import fr.yugogamer.serverinformation.server.handler.MinecraftHandler;
 import fr.yugogamer.serverinformation.server.handler.PlayerList;
 import fr.yugogamer.serverinformation.server.handler.SendMessage;
@@ -15,12 +16,16 @@ import java.net.InetSocketAddress;
 public class Server {
     private MinecraftServer minecraftServer;
 
+    private int port;
+
     public Server(MinecraftServer minecraftServer) {
         this.minecraftServer = minecraftServer;
+        this.port = Config.SERVER_PORT.get();
+        System.out.println("Server Port :"+ Config.SERVER_PORT.get());
 
         HttpServer server = null;
         try {
-            server = HttpServer.create(new InetSocketAddress(8000), 0);
+            server = HttpServer.create(new InetSocketAddress(this.port), 0);
             System.out.println("HTTP server started");
         } catch (IOException e) {
             e.printStackTrace();
